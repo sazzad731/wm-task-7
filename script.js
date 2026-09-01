28f20a4391c17da5691d14d9ac9608ee1cbd993e
sazzad731 <sajjadho731@gmail.com>
2026-09-01 18:11:36 +0600

    [MODIFIED]: slide section 2 partial

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 
 
 
-var swiper = new Swiper(".mySwiper", {
+const swiperOne = new Swiper(".swiperOne", {
   effect: "fade",
   fadeEffect: {
     crossFade: true,
   },
   speed: 1500,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".slide-section-1-pagination",
     clickable: true,
+    bulletClass: "slide-section-1-bullet",
+    bulletActiveClass: "slide-section-1-bullet-active",
     renderBullet: function (index, className) {
       return '<span class="' + className + '">' + (index + 1) + "</span>";
     },
   },
+});
+
+
+
+const swiperTow = new Swiper(".swiperTow", {
+  cssMode: true,
+  navigation: {
+    prevEl: ".swiperTow-button-next",
+    nextEl: ".swiperTow-button-prev",
+  },
+  pagination: {
+    el: ".swiperTow-pagination",
+    clickable: true,
+  },
+  keyboard: true,
 });
